0710d5c6800354c4a488cd25470a37cc605eae87
GlowingPiranha <ikimus01@gmail.com>
2025-09-02 23:14:33 +0200

    [upd] bonus-1

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,19 @@ import axios from "axios"
 
 function App() {
 
-  const [actors, setActors] = useState([]);
+  const [people, setPeople] = useState([]);
 
   const fetchActors = () => {
     axios.get("https://lanciweb.github.io/demo/api/actors/").then((resp) => {
       console.log(resp);
-      setActors(resp.data);
+      setPeople(resp.data);
+    });
+  };
+
+  const fetchActresses = () => {
+    axios.get("https://lanciweb.github.io/demo/api/actresses/").then((resp) => {
+      console.log(resp)
+      setPeople(resp.data);
     });
   };
 
@@ -20,37 +27,41 @@ function App() {
   return (
     <>
       <div className="container">
-        <div className="row">
+        <div className="row mb-3">
           <div className="col-12">
-            <button className='btn btn-primary' onClick={fetchActors}>Carica attori</button>
+            <button className='btn btn-primary me-2' onClick={fetchActors}>Load Actors</button>
+            <button className='btn btn-primary' onClick={fetchActresses}>Load Actresses</button>
           </div>
-          {actors.map((char) => {
-            return (
+          <div className="row g-3">
 
-              <div className="col-12 col-md-4 mb-3" key={char.id}>
+            {people.map((char) => {
+              return (
 
-                <div className="card h-100">
+                <div className="col-12 col-md-4 mb-3" key={char.id}>
 
-                  <img className='card-img-top card-img-style' src={char.image} alt={char.name} />
+                  <div className="card h-100">
 
-                  <div className="card-body d-flex flex-column">
+                    <img className='card-img-top card-img-style' src={char.image} alt={char.name} />
+
+                    <div className="card-body d-flex flex-column">
 
 
-                    <h4 className='card-title'>{char.name}</h4>
-                    <p className='card-text mt-auto'>
-                      <div>Nato: {char.birth_year || "unknown"}</div>
-                      <div>Nazionalità: {char.nationality || "unknown"}</div>
-                      <div>Riconoscimenti: {char.known_for ? char.known_for.join(", ") : "N/A"}</div>
-                    </p>
+                      <h4 className='card-title'>{char.name}</h4>
+                      <p className='card-text mt-auto'>
+                        <div>Date of birth: {char.birth_year || "unknown"}</div>
+                        <div>Nationality: {char.nationality || "unknown"}</div>
+                        <div>known for: {char.known_for ? char.known_for.join(", ") : "N/A"}</div>
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
 
 
 
-            );
+              );
 
-          })}
+            })}
+          </div>
         </div>
       </div >
     </>
